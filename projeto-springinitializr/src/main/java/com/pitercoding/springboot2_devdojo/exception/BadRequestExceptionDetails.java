bab6065235674758c3abe5ccb3305512a0162764
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2026-03-02 09:44:22 -0300

    feat: adiciona código da aula 18 do módulo Spring Boot 2 Essentials
    - Aula 18: Implementação de Handler Global para tratamento de exceções

---
+++ projeto-springinitializr/src/main/java/com/pitercoding/springboot2_devdojo/exception/BadRequestExceptionDetails.java
@@ -0,0 +1,16 @@
+package com.pitercoding.springboot2_devdojo.exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class BadRequestExceptionDetails {
+    private String title;
+    private int status;
+    private String details;
+    private String developerMessage;
+    private LocalDateTime timestamp;
+}
