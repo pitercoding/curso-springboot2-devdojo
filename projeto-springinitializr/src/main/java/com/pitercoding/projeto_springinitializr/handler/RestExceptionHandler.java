7d2fea1b418b542fe1a5e7f71767f561cd1b8f2f
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2026-03-03 16:31:40 -0300

    feat: adiciona código da aula 21 do módulo Spring Boot 2 Essentials
    - Aula 21: Implementação de handler para tratamento de erros de validação de campos

--- projeto-springinitializr/src/main/java/com/pitercoding/projeto_springinitializr/handler/RestExceptionHandler.java
+++ projeto-springinitializr/src/main/java/com/pitercoding/projeto_springinitializr/handler/RestExceptionHandler.java
@@ -2,25 +2,58 @@ package com.pitercoding.projeto_springinitializr.handler;
 
 import com.pitercoding.projeto_springinitializr.exception.BadRequestException;
 import com.pitercoding.projeto_springinitializr.exception.BadRequestExceptionDetails;
+import com.pitercoding.projeto_springinitializr.exception.ValidationExceptionDetails;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Log4j2
 public class RestExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException badRequestException) {
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException badRequestException) {
         return new ResponseEntity<>(
                 BadRequestExceptionDetails.builder()
                         .timestamp(LocalDateTime.now())
                         .status(HttpStatus.BAD_REQUEST.value())
-                        .title("Bad Request Exception. Check the documentation.")
+                        .title("Bad Request Exception: Check the documentation.")
                         .details(badRequestException.getMessage())
                         .developerMessage(badRequestException.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+
+        String fields = fieldErrors.stream()
+                .map(FieldError::getField)
+                .collect(Collectors.joining(", "));
+
+        String fieldsMessage = fieldErrors.stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception: Invalid Fields.")
+                        .details("Check the field(s) error.")
+                        .developerMessage(exception.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(fieldsMessage)
                         .build(), HttpStatus.BAD_REQUEST
         );
     }
--- projeto-springinitializr/src/main/java/com/pitercoding/projeto_springinitializr/requests/AnimePostRequestBody.java
+++ projeto-springinitializr/src/main/java/com/pitercoding/projeto_springinitializr/requests/AnimePostRequestBody.java
@@ -2,10 +2,10 @@ package com.pitercoding.projeto_springinitializr.requests;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 public class AnimePostRequestBody {
-
     @NotBlank(message = "Anime name cannot be blank")
     private String name;
 }
