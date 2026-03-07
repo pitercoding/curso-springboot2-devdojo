1112a1a0f9611739719dc88a38bedd3e9e945edf
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2026-03-07 09:10:48 -0300

    test: adiciona testes do Spring Data JPA (aula 30)
    Implementa testes da camada de repositório utilizando @DataJpaTest
    e banco em memória H2 conforme a aula 30 do curso Spring Boot 2 Essentials.

---
+++ projeto-springinitializr/src/test/java/com/pitercoding/projeto_springinitializr/repository/AnimeRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.pitercoding.projeto_springinitializr.repository;
+
+import com.pitercoding.projeto_springinitializr.domain.Anime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@DisplayName("Tests for Anime Repository")
+class AnimeRepositoryTest {
+
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    @DisplayName("Tests for Anime Repository")
+    void save_PersistAnime_WhenSuccessful(){
+        Anime animeToBeSaved = createAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        Assertions.assertThat(animeSaved).isNotNull();
+        Assertions.assertThat(animeSaved.getId()).isNotNull();
+        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeToBeSaved.getName());
+
+    }
+
+    private Anime createAnime() {
+        return Anime.builder().name("DBZ").build();
+    }
+}
