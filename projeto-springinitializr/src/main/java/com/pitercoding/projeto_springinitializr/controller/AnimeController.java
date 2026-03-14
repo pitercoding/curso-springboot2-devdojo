ebf0e9f6f965f4597405e6f51dd2b82da59b06c4
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2026-03-14 10:20:10 -0300

    docs(openapi): aula 46 adiciona documentação da API com SpringDoc OpenAPI pt1

--- projeto-springinitializr/src/main/java/com/pitercoding/projeto_springinitializr/controller/AnimeController.java
+++ projeto-springinitializr/src/main/java/com/pitercoding/projeto_springinitializr/controller/AnimeController.java
@@ -4,20 +4,18 @@ import com.pitercoding.projeto_springinitializr.domain.Anime;
 import com.pitercoding.projeto_springinitializr.requests.AnimePostRequestBody;
 import com.pitercoding.projeto_springinitializr.requests.AnimePutRequestBody;
 import com.pitercoding.projeto_springinitializr.service.AnimeService;
-import com.pitercoding.projeto_springinitializr.util.DateUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -29,7 +27,7 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public ResponseEntity<Page<Anime>> list(Pageable pageable) {
+    public ResponseEntity<Page<Anime>> list(@ParameterObject Pageable pageable) {
         return ResponseEntity.ok(animeService.listAll(pageable));
     }
 
